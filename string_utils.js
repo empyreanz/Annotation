f064429a10fe5e6cdf68e1315c1860bfdcc9e6c8
empyreanz <em.zeng@gmail.com>
2015-12-08 23:42:33 -0500

    clean up

--- string_utils.js
+++ string_utils.js
@@ -1,6 +1,6 @@
 
 /*
- * This file is part of Super Simple Highlighter.
+ * This file is based on Super Simple Highlighter.
  * 
  * Super Simple Highlighter is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
