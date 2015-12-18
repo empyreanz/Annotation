cec2e6bf788e17e713cab2d8b22b1c491ed39d0a
empyreanz <em.zeng@gmail.com>
2015-12-17 23:01:07 -0500

    add backend listeners

--- all.js
+++ all.js
@@ -25,22 +25,89 @@ function highlightSelection() {
 
 
 function highlightTest() {
-    console.log("click");
-    var range = _content.getSelectionRange();
-    console.log(range.toString());
-    var xPathRange = _xpath.createXPathRangeFromRange(range);
-    var range2 = _xpath.createRangeFromXPathRange(xPathRange);
-    var id = _stringUtils.createUUID({
-            beginWithLetter: true
+    chrome.extension.getBackgroundPage().console.log('foo');
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (!tabs) { return; }
+
+        var activeTab = tabs[0];
+
+        _tabs.sendGetSelectionRangeMessage(activeTab.id, function (xpathRange) {
+            if (!xpathRange) {
+                chrome.extension.getBackgroundPage().console.log('empty xPathRange');
+                return;
+            }
+            chrome.extension.getBackgroundPage().console.log(xpathRange.startContainerPath);
+            chrome.extension.getBackgroundPage().console.log(xpathRange.startOffset);
+            chrome.extension.getBackgroundPage().console.log(xpathRange.endContainerPath);
+            chrome.extension.getBackgroundPage().console.log(xpathRange.endOffset);
+            chrome.extension.getBackgroundPage().console.log(xpathRange.collapsed);
+
+            var id = _stringUtils.createUUID({
+                beginWithLetter: true
+            });
+            var className = "default-hl"
+
+            
+            try {
+                _tabs.sendCreateHighlightMessage(activeTab.id,
+                    xpathRange, className, id, function (is_created) {
+                        chrome.extension.getBackgroundPage().console.log('here');
+                        // a false response means something went wrong - delete document from db
+                        if (is_created) {
+                            // (re) show page action on success
+                            chrome.pageAction.show(tabId);
+                        } else {
+                            chrome.extension.getBackgroundPage().console.log("Error creating highlight in DOM - Removing associated document");
+                        }
+                    });
+            }catch (e){
+                console.log("Exception creating highlight in DOM - Removing associated document");
+            }
+
+            // // non collapsed selection means create new highlight
+            // if (!xpathRange.collapsed) {
+            //     // requires selection text
+            //     _tabs.sendGetRangeTextMessage(activeTab.id, xpathRange, function (selectionText) {
+            //         if (!selectionText) { return; }
+
+            //         // create new document for highlight, then update DOM
+            //         _eventPage.createHighlight(activeTab.id,
+            //             xpathRange, _database.buildMatchString(activeTab.url),
+            //             selectionText, hd.className);
+
+            //         // remove selection?
+            //         _storage.getUnselectAfterHighlight(function (unselectAfterHighlight) {
+            //             if (unselectAfterHighlight) {
+            //                 // unselect all
+            //                 _eventPage.selectHighlightText(activeTab.id);
+            //             }
+            //         });
+
+            //     });
+            // } else {
+            //     // collapsed selection range means update the hovered highlight (if possible)
+            //     var documentId = _contextMenus.getHoveredHighlightId();
+            //     if (documentId) {
+            //         _eventPage.updateHighlight(activeTab.id,
+            //             documentId, hd.className);
+            //     }
+            // }
         });
-    var className = "default-hl"
-    var hl = _content.createHighlight(xPathRange, id, className);
-    _content.selectHighlight(id);
-    console.log(xPathRange.startContainerPath);
-    console.log(xPathRange.startOffset);
-    console.log(xPathRange.endContainerPath);
-    console.log(xPathRange.endOffset);
-    console.log(xPathRange.collapsed);
+    });
+
+
+
+    // var range = _content.getSelectionRange();
+    // console.log(range.toString());
+    // var xPathRange = _xpath.createXPathRangeFromRange(range);
+    // var range2 = _xpath.createRangeFromXPathRange(xPathRange);
+    // var hl = _content.createHighlight(xPathRange, id, className);
+    // _content.selectHighlight(id);
+    // console.log(xPathRange.startContainerPath);
+    // console.log(xPathRange.startOffset);
+    // console.log(xPathRange.endContainerPath);
+    // console.log(xPathRange.endOffset);
+    // console.log(xPathRange.collapsed);
 }
 
 function getSelectionText() {
