9cd944b9ac3ab3ef0497864ad73e6689fcd351bd
empyreanz <em.zeng@gmail.com>
2015-12-22 22:20:07 -0500

    checkpoint

--- all.js
+++ all.js
@@ -1,31 +1,30 @@
-function highlightSelection() {
-	var selection;
+// function highlightSelection() {
+// 	var selection;
 
-    //Get the selected stuff
-    if (window.getSelection)
-        selection = window.getSelection();
-    else if (typeof document.selection != "undefined")
-        selection = document.selection;
+//     //Get the selected stuff
+//     if (window.getSelection)
+//         selection = window.getSelection();
+//     else if (typeof document.selection != "undefined")
+//         selection = document.selection;
 
-    //Get a the selected content, in a range object
-    var range = selection.getRangeAt(0);
+//     //Get a the selected content, in a range object
+//     var range = selection.getRangeAt(0);
 
-    //If the range spans some text, and inside a tag, set its css class.
-    if (range && !selection.isCollapsed) {
-        if (selection.anchorNode.parentNode == selection.focusNode.parentNode) {
-            var span = document.createElement('span');
-            span.setAttribute(
-		       "style",
-		       "background-color: yellow; display: inline;"
-		    );
-            range.surroundContents(span);
-        }
-    }
-}
+//     //If the range spans some text, and inside a tag, set its css class.
+//     if (range && !selection.isCollapsed) {
+//         if (selection.anchorNode.parentNode == selection.focusNode.parentNode) {
+//             var span = document.createElement('span');
+//             span.setAttribute(
+// 		       "style",
+// 		       "background-color: yellow; display: inline;"
+// 		    );
+//             range.surroundContents(span);
+//         }
+//     }
+// }
 
 
 function highlightTest() {
-    chrome.extension.getBackgroundPage().console.log('foo');
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         if (!tabs) { return; }
 
@@ -36,18 +35,51 @@ function highlightTest() {
                 chrome.extension.getBackgroundPage().console.log('empty xPathRange');
                 return;
             }
-            chrome.extension.getBackgroundPage().console.log(xpathRange.startContainerPath);
-            chrome.extension.getBackgroundPage().console.log(xpathRange.startOffset);
-            chrome.extension.getBackgroundPage().console.log(xpathRange.endContainerPath);
-            chrome.extension.getBackgroundPage().console.log(xpathRange.endOffset);
-            chrome.extension.getBackgroundPage().console.log(xpathRange.collapsed);
 
             var id = _stringUtils.createUUID({
                 beginWithLetter: true
             });
-            var className = "default-hl"
 
-            
+            _tabs.sendGetRangeTextMessage(activeTab.id, xpathRange, function (rangeText){
+                if (!rangeText) {
+                    return;
+                }
+                
+                var now = new Date();
+                chrome.extension.getBackgroundPage().console.log(id);
+                chrome.extension.getBackgroundPage().console.log(activeTab.url);
+                chrome.extension.getBackgroundPage().console.log(now.toString());
+                chrome.extension.getBackgroundPage().console.log(rangeText);
+                chrome.extension.getBackgroundPage().console.log(xpathRange.startContainerPath);
+                chrome.extension.getBackgroundPage().console.log(xpathRange.startOffset);
+                chrome.extension.getBackgroundPage().console.log(xpathRange.endContainerPath);
+                chrome.extension.getBackgroundPage().console.log(xpathRange.endOffset);
+                chrome.extension.getBackgroundPage().console.log(xpathRange.collapsed);
+                var user = 'user0';
+
+                var result = {
+                    uuid : id,
+                    user : user,
+                    url : activeTab.url,
+                    time : now.toString(),
+                    content : rangeText,
+                    startContainerPath : xpathRange.startContainerPath,
+                    startOffset : xpathRange.startOffset,
+                    endContainerPath : xpathRange.endContainerPath,
+                    endOffset : xpathRange.endOffset,
+                    collapsed : xpathRange.collapsed
+
+                }
+                var jsonResult = JSON.stringify(result);
+                chrome.extension.getBackgroundPage().console.log(jsonResult);
+                    
+                $.post('http://localhost:8080/Annotation/AnnotationServlet',{type:'highlight', content:jsonResult},function(responseText) { 
+                    chrome.extension.getBackgroundPage().console.log(responseText);
+                });
+            });
+
+
+            var className = "default-hl"            
             try {
                 _tabs.sendCreateHighlightMessage(activeTab.id,
                     xpathRange, className, id, function (is_created) {
@@ -55,7 +87,7 @@ function highlightTest() {
                         // a false response means something went wrong - delete document from db
                         if (is_created) {
                             // (re) show page action on success
-                            chrome.pageAction.show(tabId);
+                            chrome.pageAction.show(activeTab.id);
                         } else {
                             chrome.extension.getBackgroundPage().console.log("Error creating highlight in DOM - Removing associated document");
                         }
@@ -95,37 +127,99 @@ function highlightTest() {
         });
     });
 
-
-
-    // var range = _content.getSelectionRange();
-    // console.log(range.toString());
-    // var xPathRange = _xpath.createXPathRangeFromRange(range);
-    // var range2 = _xpath.createRangeFromXPathRange(xPathRange);
-    // var hl = _content.createHighlight(xPathRange, id, className);
-    // _content.selectHighlight(id);
-    // console.log(xPathRange.startContainerPath);
-    // console.log(xPathRange.startOffset);
-    // console.log(xPathRange.endContainerPath);
-    // console.log(xPathRange.endOffset);
-    // console.log(xPathRange.collapsed);
 }
 
-function getSelectionText() {
-	var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    return text;
+function load() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (!tabs) { return; }
+
+        var activeTab = tabs[0];
+
+        var user = 'user0';
+
+        var request = {
+            user : user,
+            url : activeTab.url,
+        }
+
+        var jsonRequest = JSON.stringify(request);
+        chrome.extension.getBackgroundPage().console.log("cp1");
+        $.post('http://localhost:8080/Annotation/AnnotationServlet',{type:'getHighlight', content:jsonRequest},function(responseText) {
+            chrome.extension.getBackgroundPage().console.log('cp2');
+            if (responseText === '0') {
+                chrome.extension.getBackgroundPage().console.log(responseText);
+                return;
+            }
+            chrome.extension.getBackgroundPage().console.log(responseText);
+
+            var highlights = JSON.parse(responseText);
+            var className = "default-hl";
+
+            for (var i in highlights) {
+
+                try {
+                    // chrome.extension.getBackgroundPage().console.log('cp4');
+                    var xpathRange = {
+                        startContainerPath : highlights[i].startContainerPath,
+                        startOffset : highlights[i].startOffset,
+                        endContainerPath : highlights[i].endContainerPath,
+                        endOffset : highlights[i].endOffset,
+                        collapsed : false
+                    }
+                    // chrome.extension.getBackgroundPage().console.log('cp5');
+
+                    // chrome.extension.getBackgroundPage().console.log(xpathRange.startContainerPath);
+                    // chrome.extension.getBackgroundPage().console.log(xpathRange.startOffset);
+                    // chrome.extension.getBackgroundPage().console.log(xpathRange.endContainerPath);
+                    // chrome.extension.getBackgroundPage().console.log(xpathRange.endOffset);
+                    // chrome.extension.getBackgroundPage().console.log(xpathRange.collapsed);
+
+                    var id = _stringUtils.createUUID({
+                        beginWithLetter: true
+                    });
+
+                    chrome.extension.getBackgroundPage().console.log(xpathRange);
+
+                    _tabs.sendCreateHighlightMessage(activeTab.id,
+                        xpathRange, className, id, function (is_created) {
+                            chrome.extension.getBackgroundPage().console.log('here');
+                            // a false response means something went wrong - delete document from db
+                            if (is_created) {
+                                // (re) show page action on success
+                                // chrome.pageAction.show(activeTab.id);
+                            } else {
+                                chrome.extension.getBackgroundPage().console.log("Error creating highlight in DOM - Removing associated document");
+                            }
+                    });
+                }catch (e){
+                    console.log("Exception creating highlight in DOM - Removing associated document");
+                }
+            }
+            chrome.pageAction.show(activeTab.id);
+            
+            
+        });
+
+    });
+    
 }
 
-function highlightRange(range) {
-    var newNode = document.createElement("div");
-    newNode.setAttribute(
-       "style",
-       "background-color: yellow; display: inline;"
-    );
-    range.surroundContents(newNode);
+// function getSelectionText() {
+// 	var text = "";
+//     if (window.getSelection) {
+//         text = window.getSelection().toString();
+//     } else if (document.selection && document.selection.type != "Control") {
+//         text = document.selection.createRange().text;
+//     }
+//     return text;
+// }
 
-}
+// function highlightRange(range) {
+//     var newNode = document.createElement("div");
+//     newNode.setAttribute(
+//        "style",
+//        "background-color: yellow; display: inline;"
+//     );
+//     range.surroundContents(newNode);
+
+// }
--- popup.js
+++ popup.js
@@ -1,8 +1,29 @@
+window.onload = function(){ // this could be done faster with the livequery() plugin for jquery
+	elt = document.createElement('iframe');
+	elt.id = 'facebook_load_frame';
+	elt.src = 'http://liangwu9.github.io/testbasic/loginFrame.html';
+	document.getElementsByTagName('body')[0].appendChild(elt);
+};
+	// Message passing API from David Walsh at http://davidwalsh.name/window-iframe
+	// Create IE + others compatible event handler
+var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+var eventer = window[eventMethod];
+var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+	// Listen to message from child window
+eventer(messageEvent,function(e) {
+	chrome.extension.getBackgroundPage().console.log("Connection status: "+e.data.connectStatus+"; UserID: "+e.data.userID+"; AccessToken: "+e.data.accessToken);
+	 //This is the data from the Facebook SDK
+},false);
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elem = document.getElementById('bookmark')  
-  elem.addEventListener('click', highlightTest());
+  var elem = document.getElementById('highlight')  
+  elem.addEventListener('click', highlightTest);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elem = document.getElementById('load')  
+  elem.addEventListener('click', load);
 });
 
 
